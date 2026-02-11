a1f2731c682416f8f40f79205fa5bc02049f0b16
jeancmr <jean.madiedo08@gmail.com>
2026-02-11 09:50:40 -0500

    first commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+
+bootstrap()
+  .then(() =>
+    console.log(`Server is running on port ${process.env.PORT ?? 3000}`),
+  )
+  .catch(console.error);

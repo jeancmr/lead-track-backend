7b82bf3969d3c225d356e90e332eaf211cf5efe7
jeancmr <jean.madiedo08@gmail.com>
2026-02-11 10:53:39 -0500

    feat: add typeorm and initial config for pg, add .env.template

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,44 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module, OnModuleInit } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: true,
+        ssl: configService.get<string>('DB_SSL') === 'true',
+        extra:
+          configService.get<string>('DB_SSL') === 'true'
+            ? {
+                rejectUnauthorized: false,
+              }
+            : null,
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  private readonly logger = new Logger(AppModule.name);
+
+  onModuleInit() {
+    this.logger.log('Postgres database was succesfully connected');
+  }
+}

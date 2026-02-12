034d5acc5d7e4f58061daa53eedb0ed9661854fe
jeancmr <jean.madiedo08@gmail.com>
2026-02-12 12:26:45 -0500

    feat: add login endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -9,5 +10,10 @@ export class AuthController {
   @Post('register')
   register(@Body() registerDto: RegisterDto) {
     return this.authservice.register(registerDto);
+  }
+
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authservice.login(loginDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,13 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
 import { UsersService } from 'src/users/users.service';
 import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Injectable()
 export class AuthService {
@@ -22,5 +28,28 @@ export class AuthService {
     });
 
     return { message: 'User signed up sucessfully', data: userSignedUp };
+  }
+
+  async login(loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    const userFound = await this.usersService.findOneByEmail(email);
+
+    if (!userFound) throw new NotFoundException('User does not exit');
+
+    const isPasswordMatched = await bcrypt.compare(
+      password,
+      userFound.password,
+    );
+
+    if (!isPasswordMatched) {
+      throw new UnauthorizedException('Password incorrect');
+    }
+
+    // TODO: generate JWT
+
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { password: userPassword, ...userData } = userFound;
+
+    return { message: 'User logged sucessfully', data: userData };
   }
 }
